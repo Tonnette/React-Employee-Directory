f265aeecf99c4a63c8115cb4ed6e2d151c1ce9d0
Tonnette Stanford <tonnettestanford@yahoo.com.au>
2020-03-29 12:37:25 +1100

    added more functionality

--- employee/src/App.js
+++ employee/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Search from "./pages/Search";
+// import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
+import OmdbContainer from "./components/OmdbContainer";
 
 function App() {
   return (
@@ -11,12 +12,21 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={Search} />
+          <Route exact path="/" component={OmdbContainer} />
         </Wrapper>
         <Footer />
       </div>
     </Router>
   );
 }
+
+// export default App;
+
+
+// import OmdbContainer from "./components/OmdbContainer";
+
+// function App() {
+//   return <OmdbContainer />;
+// }
 
 export default App;
