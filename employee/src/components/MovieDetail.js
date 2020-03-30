951780bfd59beb4575c14a7864d9ddce4aad37f6
Tonnette Stanford <tonnettestanford@yahoo.com.au>
2020-03-30 21:53:16 +1100

    fixed images

--- employee/src/components/MovieDetail.js
+++ employee/src/components/MovieDetail.js
@@ -3,7 +3,7 @@ import React from "react";
 function MovieDetail(props) {
   return (
     <div className="text-center">
-      {/* <img alt={props.title} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} /> */}
+      <img alt={props.title} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
       <h3>Email: {props.email}</h3>
       <h3>Phone: {props.phone}</h3>
       <h3>Country: {props.country}</h3>
--- employee/src/pages/Search.js
+++ employee/src/pages/Search.js
@@ -8,66 +8,80 @@ import MovieDetail from "../components/MovieDetail";
 import API from "../utils/API";
 
 class Search extends Component {
-  state = {
-    result: {},
-    search: ""
-  };
+    state = {
+        items: [],
+        search: ""
+    };
 
-  // When this component mounts, search for the movie "The Matrix"
 
-  componentDidMount() {
-    API.getRandom()
-      .then(res => this.setState({ result: res.data.results[0] }))
-      .catch(err => console.log(err));
-  }
-
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-  };
-
-  // When the form is submitted, search the OMDB API for the value of `this.state.search`
-//   handleFormSubmit = event => {
-//     event.preventDefault();
-//     this.searchMovies(this.state.search);
-//   };
-
-  render() {
-    return (
-      <Container>
-        <Row>
-        <Col size="md-12">
-            <Card heading="Search">
-              <SearchForm
-                value={this.state.search}
-                handleInputChange={this.handleInputChange}
-                handleFormSubmit={this.handleFormSubmit}
-              />
-            </Card>
-          </Col>
-          <Col size="md-12">
-            <Card
-              heading={ "Employee Table"}
-            >
-              {this.state.result ? (
-                <MovieDetail
-                  title={this.state.result.gender}
-                //   src={this.state.result.Poster}
-                  email={this.state.result.email}
-                  phone={this.state.result.phone}
-                  country={this.state.result.nat}
-                />
-              ) :
-              (
-                <h3>No Results to Display</h3>
-              )
-              }
-            </Card>
-          </Col>
+    componentDidMount() {
+        API.getRandom()
+            // .then((response) => response.json())
         
-        </Row>
-      </Container>
-    );
-  }
+            .then((response) => {
+                console.log(response)
+                this.setState({
+                    items: response.data.results
+                })
+            })
+            .catch(err => console.log(err))
+    }
+
+    handleInputChange = event => {
+        this.setState({ search: event.target.value });
+    };
+
+    // When the form is submitted, search the OMDB API for the value of `this.state.search`
+    //   handleFormSubmit = event => {
+    //     event.preventDefault();
+    //     this.searchMovies(this.state.search);
+    //   };
+
+    render() {
+        var { items } = this.state
+        return (
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                        <Card heading="Search">
+                            <SearchForm
+                                value={items.search}
+                                handleInputChange={this.handleInputChange}
+                                handleFormSubmit={this.handleFormSubmit}
+                            />
+                        </Card>
+                    </Col>
+                    <Col size="md-12">
+
+                        {items.map(item => (
+                            <img src={item.picture.medium} alt={item.name.first} />
+                            // <div>{item.gender}</div>
+             ))}
+
+                        <Card
+                            heading={"Employee Table"}
+                        >
+{console.log(items)}
+                            {items.length>0 ? (
+                                <MovieDetail
+                                    title={items[0].gender}
+                                      src={items[0].picture.medium} 
+                                    email={items[0].email}
+                                    phone={items[0].phone}
+                                    country={items[0].nat}
+                                />
+                            ) :
+                                (
+                                    <h3>No Results to Display</h3>
+                                )
+                            }
+                        </Card>
+                    </Col>
+
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default Search;
